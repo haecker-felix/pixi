8881381b0474e884223f311b8e4cf4f82e6a7c1f
Felix Häcker <felix@prefix.dev>
2025-11-17 10:54:17 +0100

    feat(pixi_api): Add list_packages

--- crates/pixi_api/src/context.rs
+++ crates/pixi_api/src/context.rs
@@ -15,6 +15,7 @@ use rattler_conda_types::{Channel, MatchSpec, PackageName, Platform, RepoDataRec
 use crate::interface::Interface;
 use crate::workspace::add::GitOptions;
 use crate::workspace::{DependencyOptions, InitOptions, ReinstallOptions};
+use crate::workspace::{InitOptions, Package, ReinstallOptions};
 
 pub struct DefaultContext<I: Interface> {
     _interface: I,
@@ -110,6 +111,27 @@ impl<I: Interface> WorkspaceContext<I> {
             &self.interface,
             self.workspace_mut()?,
             name,
+        )
+        .await
+    }
+
+    pub async fn list_packages(
+        &self,
+        regex: Option<String>,
+        platform: Option<Platform>,
+        environment: Option<String>,
+        explicit: bool,
+        no_install: bool,
+        lock_file_usage: LockFileUsage,
+    ) -> miette::Result<Vec<Package>> {
+        crate::workspace::list::list(
+            &self.workspace,
+            regex,
+            platform,
+            environment,
+            explicit,
+            no_install,
+            lock_file_usage,
         )
         .await
     }
